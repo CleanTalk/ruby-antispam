a81256538bf100187187e339cef72e6590762945
guille-moe <coder@guille.moe>
2017-08-04 17:33:01 +0200

    Split Class Cleantalk::Request to a separate file and refactor a bit.

--- spec/cleantalk_spec.rb
+++ spec/cleantalk_spec.rb
@@ -1,29 +1,72 @@
 require 'spec_helper'
 
 describe Cleantalk do
+  let :headers do
+    {
+      'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Content-Type' => 'application/json', 'User-Agent' => 'Ruby', 'Host'=>'moderate.cleantalk.org'
+    }
+  end
+
   describe '#is_allowed_user' do
-    it 'returns the message' do
+    it 'return the response body' do
       response = {
-        "version" => "7.47",
-        "inactive" => 0,
-        "js_disabled => 0,
-        'blacklisted" => 1,
-        "comment" => "*** Forbidden. Sender blacklisted. ***",
-        "codes" => "FORBIDDEN BL",
-        "fast_submit" => 0,
-        "id" => "5a49267e202169b3a4d9ddefee190065",
+        "version"        => "7.47",
+        "inactive"       => 0,
+        "js_disabled"    => 0,
+        "blacklisted"    => 1,
+        "comment"        => "*** Forbidden. Sender blacklisted. ***",
+        "codes"          => "FORBIDDEN BL",
+        "fast_submit"    => 0,
+        "id"             => "5a49267e202169b3a4d9ddefee190065",
         "account_status" => 1,
-        "allow" => 0
+        "allow"          => 0
       }
+
+      body = JSON.fast_generate({
+        auth_key: "your_key", method_name: "check_newuser"
+      })
+
       stub_request(:post, "https://moderate.cleantalk.org:443/api2.0").
-        with(body: "{\"method_name\":\"check_newuser\",\"auth_key\":\"your_key\"}",
-             headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'Host'=>'moderate.cleantalk.org', 'User-Agent'=>'Ruby'}).
-         to_return(status: 200, body: JSON.dump(response), headers: {})
+        with(body: body, headers: headers).
+        to_return(status: 200, body: JSON.dump(response), headers: {})
 
       request = Cleantalk::Request.new
       request.auth_key = 'your_key'
 
       expect(subject.is_allowed_user(request)).to eql(response)
+    end
+  end
+
+  describe '#is_allowed_message' do
+    it 'return the response body' do
+      response = {
+        "stop_queue"     => 0,
+        "inactive"       => 0,
+        "version"        => "7.47",
+        "spam"           => 1,
+        "js_disabled"    => 0,
+        "comment"        => "*** Forbidden. Sender blacklisted. ***",
+        "codes"          => "FORBIDDEN BL",
+        "blacklisted"    => 1,
+        "fast_submit"    => 0,
+        "account_status" => "1",
+        "id"             => "433289e278ae059f8fc58914fc890de2",
+        "allow"          => 0
+      }
+
+      body = JSON.fast_generate({
+        auth_key: "your_key", method_name: "check_message"
+      })
+
+      stub_request(:post, "https://moderate.cleantalk.org:443/api2.0").
+        with(body: body, headers: headers).
+        to_return(status: 200, body: JSON.dump(response), headers: {})
+
+      request = Cleantalk::Request.new
+      request.auth_key = 'your_key'
+
+      expect(subject.is_allowed_message(request)).to eql(response)
     end
   end
 end
