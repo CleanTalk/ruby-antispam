f30c4d68345ef3cf538745a6ccf5f57bc5fd229d
harishmallow <harish@mallow-tech.com>
2019-05-16 18:34:16 +0530

    feat: raise error if it has error no or error message

--- lib/cleantalk/result.rb
+++ lib/cleantalk/result.rb
@@ -1,11 +1,23 @@
 class Cleantalk::Result
   attr_reader :id, :version, :inactive, :js_disabled, :blacklisted,
-              :comment, :codes,:fast_submit, :account_status, :allow
+              :comment, :codes, :fast_submit, :account_status, :allow,
+              :error_no, :error_message
 
   def initialize body
     body = body.is_a?(String) ? JSON.parse(body) : body
     body.each do |meth, value|
       instance_variable_set("@#{meth}", value) if respond_to? meth
     end
+    raise_error?
   end
+
+  private
+
+  def raise_error?
+    if send(:error_no).present? || send(:error_message).present?
+      raise Cleantalk::Result::ApiErrors, "Error no: #{send(:error_no)}, Error Message: #{send(:error_message)}"
+    end
+  end
+
+  class Cleantalk::Result::ApiErrors < StandardError; end
 end
